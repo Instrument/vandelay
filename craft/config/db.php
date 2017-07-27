e197d1282cecd8369d0e2c982b94c39750d17666
Scot Mortimer <scot.mortimer@gmail.com>
2017-07-27 11:16:16 -0700

    first commit

---
+++ craft/config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'*' => array(
+		'tablePrefix' => 'craft',
+		'database' => 'snapcraft',
+		'server' => 'localhost',
+	),
+	'snapchat.craft.dev' => array(
+		'user' => 'homestead',
+		'password' => 'secret',
+	)
+);
---
+++ craft/config/general.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+  '*' => array(
+    'omitScriptNameInUrls' => true,
+    'limitAutoSlugsToAscii' => true,
+		'defaultWeekStartDay' => 0,
+		'enableCsrfProtection' => true,
+		'cpTrigger' => 'admin',
+  ),
+  'snapchat.craft.dev' => array(
+    'devMode' => true,
+    'siteUrl' => 'http://snapchat.craft.dev'
+  )
+);
